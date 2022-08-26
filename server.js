9556637ba949155b92ff21eaa6392881d94d0916
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-27 00:26:14 +0300

    server update

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 const express = require("express");
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const app = express();
 const { routerContact } = require("./src/routes");
 const { PORT, MONGO_DB } = process.env;
 
-
-const app = express();
-app.use(express.json())
-
-app.use('api/contats',routerContact)
+app.use(express.json());
+app.use("/api/contacts", routerContact);
 
 const start = async () => {
+  try {
+    await mongoose.connect(MONGO_DB);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 
-    try {
-        await mongoose.connect(MONGO_DB)
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1)
-    }
+  app.listen(PORT, () => {
+    console.log("PORT ", PORT);
+  });
+};
 
-    app.listen(PORT);
-}
-
-start()
-
-
-
-
+start();
