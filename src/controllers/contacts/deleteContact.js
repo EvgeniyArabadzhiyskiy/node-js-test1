a878bdff1a944239b864e0d0b05346a1e6515be5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-27 21:29:03 +0300

    updete files

--- src/controllers/contacts/deleteContact.js
+++ src/controllers/contacts/deleteContact.js
@@ -1,14 +1,14 @@
 const { Contact } = require("../../model/contact");
 
 const deleteContact = async (req, res) => {
-    const { id } = req.params;
-    console.log("deleteContact ~ id", id);
+  const { id } = req.params;
 
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    return res.json({ messsage: `Contact with id=${id} Not Found` });
+  }
 
-//   const result = new Contact({ ...req.body });
-//   const contact = await result.save();
-
-//   res.json({ contact, message: "success" });
+  res.json({ result, message: "success" });
 };
 
 module.exports = deleteContact;
