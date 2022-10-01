5dd0ec39d03804a61ec47593d70933de40cf11d7
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-10-01 22:04:35 +0300

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ const getAll = require('./contacts/getAll');
 const addcontact = require('./contacts/addcontact');
 const getContactById = require('./contacts/getContactById');
 const updeteContact = require('./contacts/updateContact');
+const changeContact = require('./contacts/changeContact');
 const deleteContact = require('./contacts/deleteContact');
 
 module.exports = {
@@ -9,5 +10,6 @@ module.exports = {
     addcontact,
     getContactById,
     updeteContact,
+    changeContact,
     deleteContact
 };
